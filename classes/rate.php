09423a94637ccdf139c313ef1680512d62f76633
divyanshu  rawat <divyanshu.r46956@gmail.com>
2016-10-24 00:33:03 +0530

    Implementer buttonSet function !

--- classes/rate.php
+++ classes/rate.php
@@ -49,11 +49,9 @@ class Rate
         }
 
         $sql = "SELECT *,DATE_FORMAT(date,'%d/%m/%Y') AS date_formatted FROM {$this->_table_1} WHERE active = 1 ORDER BY date DESC";
-        
-        $sql = "SELECT * FROM texts";
         $statement= $this->objDb->prepare($sql);
         $statement->execute();
-        $result = $statement->fetchAll();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
 
 
@@ -73,7 +71,7 @@ class Rate
             $sql = "SELECT * FROM {$this->_table_1} WHERE id = '$id'";
             $statement= $this->objDb->prepare($sql);
             $statement->execute();
-            $result = $statement->fetchAll();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
             return $result;
 
         }
@@ -131,7 +129,7 @@ class Rate
             $rate = $rate == 1?1:0; 
             // if rate equals to 1 then it is equal to 1 otherwise it is zero !
 
-            $sql = "INSERT INTO {$this->_table_2} ('user','item','rate1')  VALUES (?,?,?)";
+            $sql = "INSERT INTO {$this->_table_2} ('user','item','rate')  VALUES (?,?,?)";
             $statement = $this->objDb->prepare($sql);
 
             if($statement->execute(array($this->user,$id,$rate)))
@@ -173,35 +171,38 @@ class Rate
             }
 
             $sql = "SELECT * FROM {$this->_table_2} WHERE user = ? ";
+
             $statement = $this->objDb->prepare($sql);
-            $statement->execute(array($this->_user))
+            
+            $statement->execute(array($this->_user));
+            
             return $statement->fetchAll(PDO::FETCH_ASSOC);
         }
     }
+
 
     public function reset(){
         if(!empty($this->_user))
         {
             $list = $this->getAllByUser();
-        }
+        
+            if(!empty($list))
+            {
+                foreach ($list as $row) {
+                    $field = $row['rate'] == 1 ? 'up' : 'down';
+                    $this->removeRating($row['item'],$field);
+                }
 
-        if(!empty($list))
-        {
-            foreach ($list as $row) {
-                $field = $row['rate'] == 1 ? 'up' : 'down';
-                $this->removeRating($row['item'],$field);
+                $sql = "DELETE FROM {$this->_table_2} WHERE user = ?";
+                $statement = $this->objDb->prepare($sql);
+                return $statement->execute(array($this->_user));
+
             }
-
-            $sql = "DELETE FROM {$this->_table_2} WHERE user = ?";
-            $statement = $this->objDb->prepare($sql);
-            return $statement->execute(array($this->_user));
-
-        }
-        return false;
+            return false;
     }
 
     return false;
-
+}
 
     
     
@@ -209,14 +210,56 @@ class Rate
 
         if (!empty($id) && !empty($field)) {
 
-            $sql = "UPDATE {$this->_table_1} SET {$field} = {$field -1} WHERE id = ?";
+            $sql = "UPDATE {$this->_table_1} SET {$field} = {$field} - 1 WHERE id = ?";
             $statement = $this->objDb->prepare($sql);
             return $statement->execute(array($id));
         }
     }
 
 
+    public function buttonSet($id = null) {
 
+        if (!empty($id) && !empty($this->_user)) {
+        
+            $post = $this->getPost($id);
+        
+            $found = $this->getByUser($id);
+        
+            if (!empty($found)) {
+                $out  = '<div class="rateWrapper">';
+                $out .= '<span class="rateDone rateUp';
+                $out .= $found['rate'] == 1 ? ' active' : null;
+                $out .= '" data-item="';
+                $out .= $id;
+                $out .= '"><span class="rateUpN">';
+                $out .= intval($post['up']);
+                $out .= '</span></span>';
+                $out .= '<span class="rateDone rateDown';
+                $out .= $found['rate'] == 0 ? ' active' : null;
+                $out .= '" data-item="';
+                $out .= $id;
+                $out .= '"><span class="rateDownN">';
+                $out .= intval($post['down']);
+                $out .= '</span></span>';
+                $out .= '</div>';
+                return $out;
+            } else {
+                $out  = '<div class="rateWrapper">';
+                $out .= '<span class="rate rateUp" data-item="';
+                $out .= $id;
+                $out .= '"><span class="rateUpN">';
+                $out .= intval($post['up']);
+                $out .= '</span></span>';
+                $out .= '<span class="rate rateDown" data-item="';
+                $out .= $id;
+                $out .= '"><span class="rateDownN">';
+                $out .= intval($post['down']);
+                $out .= '</span></span>';
+                $out .= '</div>';
+                return $out;
+            }
+        }
+    }
 
 }
 
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once('classes/Rate.php');
+	require_once('classes/Rate.php');
 
 	$objRate = new Rate();
-	$posts = $objRate->getPost(1);
-	
+
+	$posts = $objRate->getPosts();
+
 
 ?>
-
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -15,23 +15,44 @@ require_once('classes/Rate.php');
 	<title>Thumbs up and down with PHP and jQuery</title>
 	<meta name="description" content="Thumbs up and down with PHP and jQuery" />
 	<meta name="keywords" content="Thumbs up and down with PHP and jQuery" />
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-	<link href="/css/core.css" rel="stylesheet" type="text/css" />
+	<link href="css/core.css" rel="stylesheet" type="text/css" />
 	<!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
 <body>
+
+<div id="wrapper">
+
+	<p><a href="#" class="reset">Reset</a></p>
 	
-<!--<script src="/js/jquery-1.7.1.min.js" type="text/javascript"></script>-->
+	<div id="comments">
+		
+		<?php if (!empty($posts)) { ?>
+		
+			<?php foreach($posts as $row) { ?>
+			
+				<div class="comment">
+					<span class="name">
+						Posted by <?php echo htmlentities(stripslashes($row['full_name'])); ?> on <time datetime="<?php echo date('Y-m-d', strtotime($row['date'])); ?>"><?php echo $row['date_formatted']; ?></time>
+					</span>
+					<p><?php echo htmlentities(stripslashes($row['comment'])); ?></p>
+					<?php echo $objRate->buttonSet($row['id']); ?>
+				</div>
+			
+			<?php } ?>
+		
+		<?php } else { ?>
+			<p>There are currently no comments.</p>
+		<?php } ?>
+		
+	</div>
+
+</div>
+
+
+
+<script src="/js/jquery-1.7.1.min.js" type="text/javascript"></script>
 <script src="/js/core.js" type="text/javascript"></script>
 </body>
 </html>
