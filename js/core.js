78080ca932840bdbb4a5584fde075f72d7229f78
divyanshu  rawat <divyanshu.r46956@gmail.com>
2016-10-24 18:59:03 +0530

    fixed ajax ang get_json bug in core.js !!

--- js/core.js
+++ js/core.js
@@ -1,37 +1,68 @@
 var rateObject = {
-	urlRate : '/mod/rate.php',
-	urlReset : '/mod/reset.php',
+	urlRate : 'mod/rate.php',
+	urlReset : 'mod/reset.php',
 	rate : function(obj) {
-		obj.live('click', function(e) {
+		obj.on('click', function(e) {
+
 			var thisObj = $(this);
-			var thisType = thisObj.hasClass('rateUp') ? 'up' : 'down';
-			var thisItem = thisObj.attr('data-item');
+			var thisType  = thisObj.hasClass('rateUp') ? 'up' : 'down';
+			var thisItem  = thisObj.attr('data-item');
 			var thisValue = thisObj.children('span').text();
-			jQuery.getJSON(rateObject.urlRate, { type : thisType, item : thisItem }, function(data) {
-				if (!data.error) {
-					thisObj.children('span').html(parseInt(thisValue, 10) + 1);
+			
+			var test = {
+		        type:"POST",
+		        url:"mod/rate.php",
+		        data : { item : thisItem , rate : thisType},
+		        cache: true,
+		      }
+				
+		      $.ajax(test).done(function(data){
+
+		      		thisObj.children('span').html(parseInt(thisValue, 10) + 1);
 					thisObj.parent('.rateWrapper').find('.rate').addClass('rateDone').removeClass('rate');
 					thisObj.addClass('active');
-				}
-			});
-			e.preventDefault();
+
+		       console.log(2,data);   
+
+		      }).fail(function(error){
+		      		console.log(1,error);
+		      });
+
+			   e.preventDefault();
 		});
 	},
-		reset : function(obj) {
-		obj.live('click', function(e) {
-			jQuery.getJSON(rateObject.urlReset, function(data) {
-				if (!data.error) {
+	reset : function(obj) {
+		obj.on('click', function(e) {
+
+			var test = {
+		        type:"POST",
+		        url: rateObject.urlReset,
+		        cache: true,
+		      }
+				
+		      $.ajax(test).done(function(data){
+
+		      	if (!data.error) {
 					location.reload();
 				}
-			});
+
+		       console.log(2,data);   
+
+		      }).fail(function(error){
+		      		console.log(1,error);
+		      });
+
 			e.preventDefault();
 		});
 	}
 };
-
 $(function() {
 	jQuery.ajaxSetup({ cache:false });
 	rateObject.rate($('.rate'));
 	rateObject.reset($('.reset'));
 });
+
+
+
+
 
